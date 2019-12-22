dfdcff8423d5ab8edaf8afad88d62e9f1d7d6200
Angius <koumarin@gmail.com>
2019-12-23 00:21:13 +0100

    Fixed API ports

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ let main = new Vue({
     },
     methods: {
         fetchData() {
-            axios.get('http://155.138.228.215:4000/api/get/10')
+            axios.get('http://155.138.228.215:8080/api/get/10')
                 .then((res) => {
                     this.entries = res.data;
                 })
