63146a09908a9735d54bd6cc6e40a3fb276b49e2
Angius <koumarin@gmail.com>
2019-12-23 11:23:22 +0100

    Fixed HTTPS

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ let main = new Vue({
     },
     methods: {
         fetchData() {
-            axios.get('http://155.138.228.215:8080/api/get/10')
+            axios.get('https://api.anomaly-tracker.xyz/api/get/10')
                 .then((res) => {
                     this.entries = res.data;
                 })
